1d763966287592590ac9f317fbd0fbb33bb24efd
lzhsterling <140582626+lzhsterling@users.noreply.github.com>
2023-08-20 18:29:15 +0800

    Update changebushu_Action.py

--- changebushu_Action.py
+++ changebushu_Action.py
@@ -108,5 +108,5 @@ if __name__ == "__main__":
     password = os.environ['USER_PWD']
     # step = str(randint(int(os.environ['STEP_MIN']), int(os.environ['STEP_MAX'])))
     # step = os.environ['STEP']
-    step = str(randint(12123, 25302)) 
+    step = str(randint(15123, 25302)) 
     main()
